9a844d284b936c1b8cdb9dff06d5ebb628199b80
黄铭涛 <1037827920@qq.com>
2025-02-10 05:39:27 +0000

    modify quick sort algorithm(javascript)

--- sort_algorithm/quick_sort/javascript/main.js
+++ sort_algorithm/quick_sort/javascript/main.js
@@ -1,40 +1,29 @@
 
+function quickSort(arr) {
+    if (arr.length <= 1) {
+        return arr;
+    }
 
-// 快速排序的分区函数
-function partition(arr, left, right) {
-    // 选择最右边的元素作为基准
-    let pivot = arr[right];
-    // 小于基准的元素的索引
-    let i = left;
+    // 选择基准值
+    const pivot = arr[arr.length - 1];
+    const left = [];
+    const right = [];
+    const equal = [];
 
     // 遍历数组
-    // 将小于基准的元素放到左边
-    // 将大于基准的元素放到右边
-    for (let j = left; j < right; j++) {
-        if (arr[j] < pivot) {
-            [arr[i], arr[j]] = [arr[j], arr[i]];
-            i++;
+    for (let num of arr) {
+        if (num < pivot) {
+            left.push(num);
+        } else if (num > pivot) {
+            right.push(num);
+        } else {
+            equal.push(num);
         }
     }
 
-    // 将基准元素放到正确的位置
-    [arr[i], arr[right]] = [arr[right], arr[i]];
-    return i;
+    // 对left和right进行递归排序
+    return [...quickSort(left), ...equal, ...quickSort(right)];
 }
 
-// 快速排序
-function quickSort(arr, left, right) {
-    if (left < right) {
-        // 分区操作
-        let pivotIndex = partition(arr, left, right);
-
-        // 对两边进行递归
-        quickSort(arr, left, pivotIndex - 1);
-        quickSort(arr, pivotIndex + 1, right);
-    }
-}
-
-// 测试
-let arr = [ 10, 7, 8, 9, 1, 5 ];
-quickSort(arr, 0, arr.length - 1);
-console.log(arr);
+const arr = [10, 7, 8, 9, 1, 5];
+console.log(quickSort(arr));
